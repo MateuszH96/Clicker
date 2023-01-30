064bb8ff7803a4a296598fe74b086736e74d9f95
Mateusz Hamera <mateuszhamera3@gmail.com>
2023-01-30 15:53:47 +0100

    naprawa błędów

--- index.js
+++
@@ -1,6 +0,0 @@
-// Access DOM element object
-const rotated = document.getElementById('rotated');
-function rotateImage() {
-  // Rotate image by 90 degrees clockwise
-  rotated.style.transform = 'rotate(90deg)';
-}
