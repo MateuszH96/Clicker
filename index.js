11d885213afd3bf659805b6df9f18c6494109285
Polibus <115350848+Polibus@users.noreply.github.com>
2023-01-30 15:15:32 +0100

    śnieżka

---
+++ index.js
@@ -0,0 +1,6 @@
+// Access DOM element object
+const rotated = document.getElementById('rotated');
+function rotateImage() {
+  // Rotate image by 90 degrees clockwise
+  rotated.style.transform = 'rotate(90deg)';
+}
