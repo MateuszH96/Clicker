5ee96fc0eb99cc2ae5b24cbb7f5d87f85beb172f
Mateusz Hamera <mateuszhamera3@gmail.com>
2023-01-30 19:43:05 +0100

    aktualizacja nazw funkcji

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var money = document.getElementById("money")
 var autoclick = false
 var moneyPerClick = 1
 var itemBuy =[
-    {name:'Bąbka',photo:'bauble_',price: 100,   num:0,value:1},
-    {name:'Dzwonek',photo:'bells_',price: 500,   num:0,value:10},
-    {name:'Elf',photo:'Elf_',price: 1000,  num:0,value:50},
-    {name:'Ciastek',photo:'Gingerbread_',price: 1500,  num:0,value:100},
-    {name:'Renifer',photo:'Renifer_',price: 2000,  num:0,value:200},
+    {name:'Bąbka',  photo:'bauble_',        price: 100,   num:0,    value:1     },
+    {name:'Dzwonek',photo:'bells_',         price: 500,   num:0,    value:10    },
+    {name:'Elf',    photo:'Elf_',           price: 1000,  num:0,    value:50    },
+    {name:'Ciastek',photo:'Gingerbread_',   price: 1500,  num:0,    value:100   },
+    {name:'Renifer',photo:'Renifer_',       price: 2000,  num:0,    value:200   },
 ]
-function press(){
+function pressFlake(){
     money.innerHTML = parseFloat(money.innerHTML)+ moneyPerClick
 }
 
@@ -18,8 +18,8 @@ function autoPressFun(){
     }
 }
 
-function test(input){
-    moneyPerClick+=input
+function buyItem(input){
+    moneyPerClick += itemBuy[input].value
 }
 
 autoclickInterval = window.setInterval(autoPressFun, 1000);
