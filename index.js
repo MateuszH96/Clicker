55a0f0add94517c3e79c093f2a16acb8c84cc791
Mateusz Hamera <mateuszhamera3@gmail.com>
2023-01-30 18:53:03 +0100

    dodanie kodu

---
+++ index.js
@@ -0,0 +1,25 @@
+var money = document.getElementById("money")
+var autoclick = false
+var moneyPerClick = 1
+var itemBuy =[
+    {name:'Bąbka',photo:'bauble_',price: 100,   num:0,value:1},
+    {name:'Dzwonek',photo:'bells_',price: 500,   num:0,value:10},
+    {name:'Elf',photo:'Elf_',price: 1000,  num:0,value:50},
+    {name:'Ciastek',photo:'Gingerbread_',price: 1500,  num:0,value:100},
+    {name:'Renifer',photo:'Renifer_',price: 2000,  num:0,value:200},
+]
+function press(){
+    money.innerHTML = parseFloat(money.innerHTML)+ moneyPerClick
+}
+
+function autoPressFun(){
+    if(autoclick){
+        press()
+    }
+}
+
+function test(input){
+    moneyPerClick+=input
+}
+
+autoclickInterval = window.setInterval(autoPressFun, 1000);
